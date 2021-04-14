6fe93a9269b30cfe4b2a4928b50da598fe41e5f6
Maksuedson <maksuedson@hotmail.com>
2021-04-14 20:23:49 -0300

    created pageable

--- src/main/java/com/maksuedson/aulajparepository/controller/UserController.java
+++ src/main/java/com/maksuedson/aulajparepository/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.maksuedson.aulajparepository.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,12 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		List<User> result = repo.findAll();
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<User>> findAll(Pageable pageable){
+		Page<User> result = repo.findAll(pageable);
 		return ResponseEntity.ok(result);
 	}
 }
